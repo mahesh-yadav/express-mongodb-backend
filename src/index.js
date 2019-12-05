7e614933a3359046208b650db4885d1e44f3dc7f
Mahesh Yadav <mahesh.iitmandi@gmail.com>
2019-12-05 09:50:00 +0530

    Intial mongodb connection setup

--- src/index.js
+++ src/index.js
@@ -1,8 +1,32 @@
-// require('dotenv').config();
 import app from './app';
+import { MongoClient } from 'mongodb';
 
 const PORT = process.env.PORT || 4000;
 
-app.listen(PORT, () => {
-  console.log(`Server running at localhost:${PORT}.....`);
+const client = new MongoClient(process.env.MONGODB_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+client
+  .connect()
+  .then((client) => {
+    console.log('Connected to Database....');
+
+    app.listen(PORT, () => {
+      console.log(`Server running at localhost:${PORT}.....`);
+    });
+  })
+  .catch((err) => {
+    console.log('Failed to connect to MongoDB Server', err.message);
+  });
+
+process.on('SIGINT', async () => {
+  try {
+    await client.close();
+  } catch (err) {
+    console.log(err);
+  } finally {
+    process.exit(1);
+  }
 });
